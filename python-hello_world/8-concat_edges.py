5a09c88c0f61fa7b521b3c097267fd8a5a76981f
bz888 <ziyu.ben158@gmail.com>
2026-05-10 14:24:04 +1000

    fix 8

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 
-str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
-print(str[39:67] + str[107:112] + str[:6])
+text = (
+    "Python is an interpreted, interactive, object-oriented programming "
+    "language that combines remarkable power with very clear syntax"
+)
+
+print(text[39:67] + text[107:112] + text[:6])
