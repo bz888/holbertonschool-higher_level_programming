b4bc674229c867804456d82d2834388432d96cd7
bz888 <ziyu.ben158@gmail.com>
2026-05-13 15:48:37 +1000

    0-5

---
+++ python-data_structures/0-print_list_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def print_list_integer(my_list=[]):
+    """Print all integers of a list.
+
+    Args:
+        my_list (list): A list of integers.
+    """
+    for i in range(len(my_list)):
+        print("{:d}".format(my_list[i]))
