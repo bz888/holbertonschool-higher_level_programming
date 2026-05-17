b14b278ce83ecf88ec1aec4703cbffd28de76e0e
bz888 <ziyu.ben158@gmail.com>
2026-05-17 23:42:32 +1000

    3

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module that provides square printing."""
+
+
+def print_square(size):
+    """Print a square with the # character."""
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for _ in range(size):
+        print("#" * size)
