61d09f3db1aec390a69157ba7bac6f6215a302f1
bz888 <ziyu.ben158@gmail.com>
2026-05-17 23:35:21 +1000

    0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,7 +1,20 @@
 #!/usr/bin/python3
+"""Module that provides integer addition."""
 
 
 def add_integer(a, b=98):
+    """Return the integer addition of a and b.
+
+    Args:
+        a: First integer or float.
+        b: Second integer or float. Defaults to 98.
+
+    Raises:
+        TypeError: If a or b is not an integer or float.
+
+    Returns:
+        The sum of a and b after casting floats to integers.
+    """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
