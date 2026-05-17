d4c71d095ea2993a5916ac1296bfae305cd22940
bz888 <ziyu.ben158@gmail.com>
2026-05-17 21:53:23 +1000

    0,1

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-	if not matrix:
-		return None
-	return [[i ** 2 for i in row] for row in matrix]
+    if not matrix:
+        return None
+    return [[i ** 2 for i in row] for row in matrix]
