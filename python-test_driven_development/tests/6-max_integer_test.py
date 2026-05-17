a8b67cc997e3c3fc4697a0dd09f7ded98f18f231
bz888 <ziyu.ben158@gmail.com>
2026-05-17 23:48:21 +1000

    5

---
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""Unittests for max_integer function."""
+
+import unittest
+
+max_integer = __import__('6-max_integer').max_integer
+
+
+class TestMaxInteger(unittest.TestCase):
+    """Test cases for max_integer."""
+
+    def test_ordered_list(self):
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+
+    def test_unordered_list(self):
+        self.assertEqual(max_integer([4, 1, 3, 2]), 4)
+
+    def test_max_at_beginning(self):
+        self.assertEqual(max_integer([9, 1, 2, 3]), 9)
+
+    def test_max_at_end(self):
+        self.assertEqual(max_integer([1, 2, 3, 9]), 9)
+
+    def test_one_element(self):
+        self.assertEqual(max_integer([7]), 7)
+
+    def test_empty_list(self):
+        self.assertIsNone(max_integer([]))
+
+    def test_negative_numbers(self):
+        self.assertEqual(max_integer([-5, -2, -10]), -2)
+
+    def test_mixed_numbers(self):
+        self.assertEqual(max_integer([-5, 0, 12, -1]), 12)
+
+    def test_duplicate_max(self):
+        self.assertEqual(max_integer([1, 3, 3, 2]), 3)
+
+    def test_float_values(self):
+        self.assertEqual(max_integer([1.5, 3.2, 2.8]), 3.2)
+
+
+if __name__ == "__main__":
+    unittest.main()
