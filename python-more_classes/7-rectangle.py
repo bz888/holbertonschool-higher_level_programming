3c72446c370e05784f60db3396b9b8f718ede2b0
bz888 <ziyu.ben158@gmail.com>
2026-05-24 14:08:55 +1000

    7

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -58,7 +58,10 @@ class Rectangle:
         """Return the printable rectangle representation."""
         if self.__width == 0 or self.__height == 0:
             return ""
-        return "\n".join(str(self.print_symbol) * self.__width for _ in range(self.__height))
+        return "\n".join(
+            str(self.print_symbol) * self.__width
+            for _ in range(self.__height)
+        )
 
     def __repr__(self):
         """Return a string representation that can recreate the rectangle."""
