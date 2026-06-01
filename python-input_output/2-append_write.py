7f39213e9bcb3a4759c0f52df6e5f37abc1a85e2
bz888 <ziyu.ben158@gmail.com>
2026-06-01 12:55:46 +1000

    2

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -3,6 +3,6 @@
 
 
 def append_write(filename="", text=""):
-    """Append text to a UTF-8 file and return the number of characters added."""
+    """Append """
     with open(filename, "a", encoding="utf-8") as file:
         return file.write(text)
