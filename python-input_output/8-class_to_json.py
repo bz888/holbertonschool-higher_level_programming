401b39c3f351d32e2e7161c2480defcdbb3f8846
bz888 <ziyu.ben158@gmail.com>
2026-06-01 13:09:06 +1000

    8

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module for converting an object to a JSON-serializable dictionary."""
+
+
+def class_to_json(obj):
+    """Return the dictionary description of an object for JSON serialization."""
+    return obj.__dict__
