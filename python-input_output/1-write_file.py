7044653814ae1aa497903dda20c489d92ed1337b
bz888 <ziyu.ben158@gmail.com>
2026-06-01 12:53:20 +1000

    fix 1

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -3,6 +3,6 @@
 
 
 def write_file(filename="", text=""):
-    """Write text to a UTF-8 file and return the number of characters written."""
+    """Write text to a UTF-8 file, return len."""
     with open(filename, "w", encoding="utf-8") as file:
         return file.write(text)
