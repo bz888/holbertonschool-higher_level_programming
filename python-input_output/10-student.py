07f272a93e101683a7b7dcd5e007cdfa84180c6c
bz888 <ziyu.ben158@gmail.com>
2026-06-01 13:12:08 +1000

    10

---
+++ python-input_output/10-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Module defining a Student class."""
+
+
+class Student:
+    """Represent a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a Student instance."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Return a dictionary representation of the Student"""
+        if type(attrs) is list and all(type(attr) is str for attr in attrs):
+            return {
+                attr: getattr(self, attr)
+                for attr in attrs
+                if hasattr(self, attr)
+            }
+
+        return self.__dict__
